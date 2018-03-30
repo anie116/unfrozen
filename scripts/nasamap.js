499c4fbda2d2e4bc8f9f39eee0cfbfffea346b21
Jack <shiungchenyu@gmail.com>
2018-03-30 17:57:09 +0800

    fix nasa 動畫

--- scripts/nasamap.js
+++ scripts/nasamap.js
@@ -2,9 +2,10 @@
 var t = 2018;
 //地圖移動
 var m = 0;
+//點擊次數
+var c = 0;
 //顯示倒數秒數
 function showYear(){
-    t -= 1;
     document.getElementById('showYear').innerHTML= t;
     if(t>=1945){
       setTimeout("showYear()",100);
@@ -13,12 +14,27 @@ function showYear(){
         }
     }else{
         t=1945;
+        c =2;
         document.getElementById('showYear').innerHTML= t;
         setTimeout("location.href='#teaser';",2000);
     }
+    t -= 1;
 }
 function mapAnimate(){
     $("#nasa-animation").css({"animation":"nasazoom 8s  both"});
     // $("#nasa-animation").css({"transform":"translate(  -2545px ,570px) scale(4)"});
     m++;
 }
+$("#nasaTrigger").unbind('click').click(function(){
+    $(window).scroll(function(){
+        var navHeight = $("nav").height();
+        var nasaTop = $("section#nasa").offset().top - navHeight;
+        if( $(window).scrollTop() > nasaTop && c == 0 ){
+            showYear();
+            c = 1;
+        }
+    });
+    if( c == 2){
+         setTimeout("location.href='#teaser';",2000);
+    }
+});
